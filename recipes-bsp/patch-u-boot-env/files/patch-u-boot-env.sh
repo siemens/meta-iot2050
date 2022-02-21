a9cde7d0b8d7055eeda99785cb600af62bd9c8cd
Jan Kiszka <jan.kiszka@siemens.com>
2022-02-21 19:52:58 +0100

    patch-u-boot-env: Simplify and cleanup service/script interface
    Pass the key for detecting already applied patch from the service file,
    rather than hard-coding it into the service script. While at it, remove
    useless local vars from the script and use 'fw_printenv -n' to output
    only the var value.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- recipes-bsp/patch-u-boot-env/files/patch-u-boot-env.sh
+++ recipes-bsp/patch-u-boot-env/files/patch-u-boot-env.sh
@@ -6,14 +6,9 @@
 #  Quirin Gylstorff <quirin.gylstorff@siemens.com>
 #
 # SPDX-License-Identifier: MIT
-SCRIPT="$1"
-# use grep and cut to strip away environment status and get the value
-# of sysselect
-key="sysselect"
-sysselect_value=$(fw_printenv "${key}" | grep "${key}" | cut -d= -f2)
 
-if [ -z "${sysselect_value}" ]; then
-    if ! fw_setenv -s "${SCRIPT}"; then
+if [ -z "$(fw_printenv -n "$1")" ]; then
+    if ! fw_setenv -s "$2"; then
         echo "$0: could not patch u-boot firmware"
         exit 1
     fi
