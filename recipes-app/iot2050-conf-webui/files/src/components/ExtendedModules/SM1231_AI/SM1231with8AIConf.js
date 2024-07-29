b6dbc14f2bec1b54a5ff524396e80814e6e278f2
Li Hua Qian <huaqian.li@siemens.com>
2024-07-29 14:52:04 +0800

    iot2050-conf-webui: Add support for sm1231 4AI module
    Signed-off-by: Li Hua Qian <huaqian.li@siemens.com>

---
+++ recipes-app/iot2050-conf-webui/files/src/components/ExtendedModules/SM1231_AI/SM1231with8AIConf.js
@@ -0,0 +1,44 @@
+/* eslint-disable camelcase */
+/* eslint-disable react/prop-types */
+import * as React from 'react';
+import SM1231AIConf, { channelConfigDefault } from './SM1231AIConf';
+import uiString from '@/lib/uiString/SM1231_AI.json';
+
+export const SM1231with8AIConfDefault = {
+  mlfb: '6ES7231-4HF32-0XB0',
+  power_alarm: {
+    label: uiString.LABEL_POWER_ALARM,
+    value: true
+  },
+  integ_time: {
+    label: uiString.LABEL_INT_TIME,
+    selection: [
+      uiString.INT_TIME_0,
+      uiString.INT_TIME_1,
+      uiString.INT_TIME_2,
+      uiString.INT_TIME_3
+    ],
+    value: uiString.INT_TIME_2
+  },
+  channels: [
+    JSON.parse(JSON.stringify(channelConfigDefault)),
+    JSON.parse(JSON.stringify(channelConfigDefault)),
+    JSON.parse(JSON.stringify(channelConfigDefault)),
+    JSON.parse(JSON.stringify(channelConfigDefault)),
+    JSON.parse(JSON.stringify(channelConfigDefault)),
+    JSON.parse(JSON.stringify(channelConfigDefault)),
+    JSON.parse(JSON.stringify(channelConfigDefault)),
+    JSON.parse(JSON.stringify(channelConfigDefault))
+  ]
+};
+
+export default function SM1231with8AIConf ({ slotNum, configData, updateConfig }) {
+  return (
+    <SM1231AIConf
+      slotNum={slotNum}
+      configData={configData}
+      updateConfig={updateConfig}
+      chTotal={8}
+    />
+  );
+}
